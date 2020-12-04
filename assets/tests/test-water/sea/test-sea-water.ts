dbd4f810530cde229c29d568175ee9bbcd562c10
2youyou2 <501251991@qq.com>
2020-12-04 15:47:48 +0800

    support buoyancy

--- assets/tests/test-water/sea/test-sea-water.ts
+++ assets/tests/test-water/sea/test-sea-water.ts
@@ -1,30 +1,56 @@
-import { _decorator, Component, Node, director, LabelComponent } from 'cc';
+import { _decorator, Component, Node, director, LabelComponent, Canvas, find, instantiate, Vec3, systemEvent, SystemEventType } from 'cc';
 const { ccclass, property, type } = _decorator;
 
 @ccclass('TestWater')
 export class TestWater extends Component {
-    /* class member could be defined like this */
-    // dummy = '';
-
-    /* use `property` decorator if your want the member to be serializable */
-    // @property
-    // serializableDummy = 0;
-
     @property({
         animatable: true
     })
     get skyIllum () {
-        return director.getRunningScene().globals.ambient.skyIllum;
+        return director.getScene()!.globals.ambient.skyIllum;
     }
     set skyIllum (v) {
-        director.getRunningScene().globals.ambient.skyIllum = v;
+        director.getScene()!.globals.ambient.skyIllum = v;
     }
 
     @type(LabelComponent)
-    skyIllumLabel: LabelComponent = null;
+    skyIllumLabel: LabelComponent | null = null;
+
+    @type(Node)
+    dynamicTemplates: Node | null = null;
+
+    @type(Node)
+    dynamicRoot: Node | null = null;
 
     start () {
         // Your initialization goes here.
+        for (let i = 0; i < 5; i++) {
+            this.generateDynamic();
+        }
+    }
+
+    generateDynamic () {
+        if (!this.dynamicTemplates || !this.dynamicRoot) {
+            return;
+        }
+        let templates = this.dynamicTemplates.children;
+
+        let index = Math.floor(Math.random() * templates.length);
+        let template = templates[index];
+
+        if (!template) {
+            return;
+        }
+
+        let node = instantiate(template);
+        node.position = Vec3.ZERO;
+
+        let scale = node.getScale();
+        let randomScale = 0.5 + Math.random();
+        scale.multiplyScalar(randomScale);
+        node.scale = scale;
+
+        node.parent = this.dynamicRoot;
     }
 
     update (deltaTime: number) {
